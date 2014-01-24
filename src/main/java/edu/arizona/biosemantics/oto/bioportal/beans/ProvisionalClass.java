9b661fd7bbaa654fa2352271c2c20c3d15e47673
rodenhausen <thomas.rodenhausen@gmail.com>
2014-01-24 11:03:45 -0700

    update API

---
+++ src/main/java/edu/arizona/biosemantics/oto/bioportal/beans/ProvisionalClass.java
@@ -0,0 +1,148 @@
+package edu.arizona.biosemantics.oto.bioportal.beans;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonAnySetter;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class ProvisionalClass {
+
+	private static ObjectMapper objectMapper = new ObjectMapper();
+	private String label;
+	private List<String> synonym;
+	private List<String> definition;
+	private String subclassOf;
+	private String creator;
+	private String created;
+	private String permanentId;
+	private String noteId;
+	private List<String> ontology;
+	private String id;
+	private String type;
+	
+	public ProvisionalClass() { }
+
+	
+	public ProvisionalClass(String label, List<String> synonym,
+			List<String> definition, String subclassOf, String creator,
+			String created, String permanentId, String noteId,
+			List<String> ontology, String id, String type) {
+		super();
+		this.label = label;
+		this.synonym = synonym;
+		this.definition = definition;
+		this.subclassOf = subclassOf;
+		this.creator = creator;
+		this.created = created;
+		this.permanentId = permanentId;
+		this.noteId = noteId;
+		this.ontology = ontology;
+		this.id = id;
+		this.type = type;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public List<String> getSynonym() {
+		return synonym;
+	}
+
+	public void setSynonym(List<String> synonym) {
+		this.synonym = synonym;
+	}
+
+	public List<String> getDefinition() {
+		return definition;
+	}
+
+	public void setDefinition(List<String> definition) {
+		this.definition = definition;
+	}
+
+	public String getSubclassOf() {
+		return subclassOf;
+	}
+
+	public void setSubclassOf(String subclassOf) {
+		this.subclassOf = subclassOf;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public String getPermanentId() {
+		return permanentId;
+	}
+
+	public void setPermanentId(String permanentId) {
+		this.permanentId = permanentId;
+	}
+
+	public String getNoteId() {
+		return noteId;
+	}
+
+	public void setNoteId(String noteId) {
+		this.noteId = noteId;
+	}
+
+	public List<String> getOntology() {
+		return ontology;
+	}
+
+	public void setOntology(List<String> ontology) {
+		this.ontology = ontology;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	@JsonProperty("@id")
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	@JsonProperty("@type")
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	@JsonAnySetter
+	public void handleUnknown(String key, Object value) { }
+		
+	@Override
+	public String toString() {
+		try {
+			return objectMapper.writeValueAsString(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+}
---
+++ src/main/java/edu/arizona/biosemantics/oto/bioportal/client/FilterCriteria.java
@@ -0,0 +1,7 @@
+package edu.arizona.biosemantics.oto.bioportal.client;
+
+public interface FilterCriteria<T> {
+
+	public boolean filter(T t);
+	
+}
